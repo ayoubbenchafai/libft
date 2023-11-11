b207efc1393a5407118e4a4b22c7a49000fbf7cb
ayoubbenchafai <ayoubbenchafai3@gmail.com>
2023-11-11 12:44:55 +0100

    My library

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: aben-cha <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 18:15:37 by aben-cha          #+#    #+#             */
-/*   Updated: 2023/11/09 18:18:49 by aben-cha         ###   ########.fr       */
+/*   Updated: 2023/11/11 12:40:36 by aben-cha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	i = 0;
 	s = (unsigned char *)src;
 	d = (unsigned char *)dest;
+	if (!s && !d)
+		return (0);
 	while (i < n)
 	{
 		d[i] = s[i];
