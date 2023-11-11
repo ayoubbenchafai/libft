17bdc212450198a7f5449157af3315bfeca0c25d
ayoubbenchafai <ayoubbenchafai3@gmail.com>
2023-11-11 11:36:03 +0100

    update

--- ft_split.c
+++ ft_split.c
@@ -6,72 +6,77 @@
 /*   By: aben-cha <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 20:05:41 by aben-cha          #+#    #+#             */
-/*   Updated: 2023/11/10 21:29:25 by aben-cha         ###   ########.fr       */
+/*   Updated: 2023/11/11 11:32:55 by aben-cha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int    nbr_strings(const char *s, char c)
+static int	nbr_strings(const char *s, char c)
 {
-    int    len;
+	int	len;
 
-    len = 0;
-    while (*s)
-    {
-        while (*s && *s == c)
-            s++;
-        if (*s)
-            len++;
-        while (*s && *s != c)
-            s++;
-    }
-    return (len);
+	len = 0;
+	while (*s)
+	{
+		while (*s && *s == c)
+			s++;
+		if (*s)
+			len++;
+		while (*s && *s != c)
+			s++;
+	}
+	return (len);
 }
 
-static void free_memory(char **ptr, int j)
+static void	free_memory(char **ptr, int j)
 {
-    while (j > 0)
-        free(ptr[--j]);
-    free(ptr);
+	while (j)
+	{
+		j--;
+		free(ptr[j]);
+	}
+	free(ptr);
 }
 
-static char **loading(char const *s, char c, int size, int j)
+static char	**ft_allocate_words(char const *s, char c, int size, int j)
 {
-    int i;
-    char **ptr;
-    
-    ptr = (char **)malloc(sizeof(char *) * (size + 1));
-    if (!ptr)
-        return (0);
-    while (j < size) {
-        while (*s != '\0' && *s == c)
-            s++;
-        i = 0;
-        while (s[i] != '\0' && s[i] != c)
-            i++;
-        ptr[j] = ft_substr(s, 0, i);
-        if (!ptr[j])
-        {
-            free_memory(ptr, j);
-            return (0);
-        }
-        s += i;
-        j++;
-    }
-    ptr[j] = (0);
-    return ptr;
+	int		i;
+	char	**ptr;
+
+	ptr = (char **)malloc(sizeof(char *) * (size + 1));
+	if (!ptr)
+		return (0);
+	while (j < size)
+	{
+		while (*s != '\0' && *s == c)
+			s++;
+		i = 0;
+		while (s[i] != '\0' && s[i] != c)
+			i++;
+		ptr[j] = ft_substr(s, 0, i);
+		if (!ptr[j])
+		{
+			free_memory(ptr, j);
+			return (0);
+		}
+		s += i;
+		j++;
+	}
+	ptr[j] = (0);
+	return (ptr);
 }
 
-char **ft_split(char const *s, char c)
+char	**ft_split(char const *s, char c)
 {
-    int j;
-    int size;
-    if (!s)
-        return NULL;
-    j = 0;
-    size = nbr_strings(s, c);
-    char **ptr = loading(s, c, size, j);
-    
-    return ptr;
+	int		j;
+	int		size;
+	char	**ptr;
+
+	if (!s)
+		return (0);
+	size = nbr_strings(s, c);
+	j = 0;
+	ptr = ft_allocate_words(s, c, size, j);
+	return (ptr);
 }
