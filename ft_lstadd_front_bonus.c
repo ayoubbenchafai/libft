d7c2778a63faf0b4542773b641fc6ebd52292b0d
ayoubbenchafai <ayoubbenchafai3@gmail.com>
2023-11-17 11:15:04 +0100

    update

--- ft_lstadd_front_bonus.c
+++ ft_lstadd_front_bonus.c
@@ -6,7 +6,7 @@
 /*   By: aben-cha <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 15:58:43 by aben-cha          #+#    #+#             */
-/*   Updated: 2023/11/16 15:59:01 by aben-cha         ###   ########.fr       */
+/*   Updated: 2023/11/17 11:13:37 by aben-cha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	if (!lst && !new)
+	if (!lst || !new)
 		return ;
 	new -> next = *lst;
 	*lst = new;
